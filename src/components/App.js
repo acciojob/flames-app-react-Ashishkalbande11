7391bda5a3df11f3f6bbbf7de46f2a301466b550
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-10-15 14:58:08 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,74 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from 'react'
 
-class App extends Component {
-    render() {
+const outputs = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+const App = () => {
+
+    const[fn, setfn] = useState('');
+    const[ln, setLn] = useState("");
+    const[relation, setRelation] = useState("");
+
+
+    const calculateBtnHandler = () => {
+        if(fn.length == 0 || ln.length == 0){
+            return setRelation("Please Enter valid input");
+        }
+
+        let firstArr = fn.split("");
+        let secondArr = ln.split("");
+
+        let nArr = [];
+
+        for(let i=0; i<firstArr.length; i++){
+            let flag = false;
+            for(let j = 0; j<secondArr.length; j++){
+                if(firstArr[i] == secondArr[j]){
+                    flag = true;
+                    secondArr.splice(j,1);
+                    break;
+                }
+            }
+
+            if(!flag){
+                nArr.push(firstArr[i]);
+            }
+        }
+
+        let score = (nArr.length + secondArr.length)%6;
+
+        switch(score){
+            case 1 :
+                return setRelation("Friends");
+            case 2 :
+                return setRelation("Love");
+            case 3 :
+                return setRelation("Friends");
+            case 4 :
+                return setRelation("Marraige");
+            case 5 :
+                return setRelation("Enemy");
+            case 0 :
+                return setRelation("Siblings");
+        }
     }
+  return (
+    <div>
+        <input name="name1" data-testid="input1" value="fn" onChange = {(e) => {
+            setfn(e.target.value); 
+        }} placeholder="EnterFirstName" />
+        <input name="name2" data-testid="input2" value="fn" onChange = {(e) => {
+            setLn(e.target.value); 
+        }} placeholder="EnterSecondName" />
+         <button data-testid="calculate_relationship" onClick={calculateBtnHandler}>Calculate Relationship Future</button>
+        <button data-testid="clear" onClick={() => {
+            setfn("")
+            setLn("")
+            setRelation("")
+        }}>Clear</button>
+
+        <h3 data-testid="answer">{relation}</h3>
+    </div>
+  )
 }
 
-
-export default App;
+export default App
